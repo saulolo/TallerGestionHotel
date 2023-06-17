d4dff80c987480c6dfd7c77c8c5f79a1ab099e04
Saul Echeverri <secheverri@cidenet.com.co>
2023-06-16 19:21:53 -0500

    Creando mi hola mundo

--- src/main/java/edu/TdeA/hotel/HotelApplication.java
+++ src/main/java/edu/TdeA/hotel/HotelApplication.java
@@ -2,13 +2,22 @@ package edu.TdeA.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class HotelApplication {
+
+	@GetMapping("/hello")
+	public String GetSaludar() {
+		return "Hola mundo...Saldremos vivos de esta...";
+	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
+
 	}
-
-
 }
