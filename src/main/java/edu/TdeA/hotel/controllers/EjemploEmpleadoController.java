bc324b0c5159df1c5eb3c85976964ca0a82d8cc4
Saul Echeverri <secheverri@cidenet.com.co>
2023-06-22 11:17:26 -0500

    Creando el controlador con @Controller como ejemplo para trabajar con thymelief

---
+++ src/main/java/edu/TdeA/hotel/controllers/EjemploEmpleadoController.java
@@ -0,0 +1,30 @@
+package edu.TdeA.hotel.controllers;
+
+import edu.TdeA.hotel.entities.Empleado;
+import edu.TdeA.hotel.services.EmpleadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class EjemploEmpleadoController {
+
+    /*Este es un ejemplo de como se mapea un servicio con Controller*/
+
+    @Autowired
+    private EmpleadoService empleadoService;
+
+    @GetMapping({"/Empleados","/VerEmpleados"})
+    public String viewEmpleados(Model model) {
+        List<Empleado> empleadoList = empleadoService.getAllEmpleados();
+        model.addAttribute("emplist", empleadoList);
+        return "VerEmpleados";
+    }
+
+    //terminar los métodos de guardar, editar y eliminat empleados aqui en el controlador
+
+
+}
